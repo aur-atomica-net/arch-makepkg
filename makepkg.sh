dca4a3f45bc8c267efa1ce34c26545355040bdde
Jason R. McNeil <jason@jasonrm.net>
2016-05-07 22:35:22 -0700

    Skip http proxy cache?

--- makepkg.sh
+++ makepkg.sh
@@ -12,9 +12,9 @@ sudo pacman --sync --sysupgrade --refresh --noconfirm
 
 MAKEFLAGS="-J$(nproc)" makepkg --force --noconfirm --syncdeps --install --nocheck --sign
 
-curl -L -o ${REPO}.db.tar.gz http://aur.atomica.net/atomica/x86_64/${REPO}.db
+http_proxy="" curl -L -o ${REPO}.db.tar.gz http://aur.atomica.net/atomica/x86_64/${REPO}.db
 sha256sum ${REPO}.db.tar.gz
-curl -L -o ${REPO}.db.tar.gz.sig http://aur.atomica.net/atomica/x86_64/${REPO}.db.sig
+http_proxy="" curl -L -o ${REPO}.db.tar.gz.sig http://aur.atomica.net/atomica/x86_64/${REPO}.db.sig
 repo-add --sign --verify ${REPO}.db.tar.gz *.pkg.tar.xz
 sha256sum ${REPO}.db.tar.gz
 
