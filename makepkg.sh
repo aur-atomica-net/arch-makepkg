ce9ed96e5d898706252809b230f03ef570642536
Jason R. McNeil <jason@jasonrm.net>
2016-05-07 22:23:39 -0700

    Log SHA 256 of database

--- makepkg.sh
+++ makepkg.sh
@@ -13,6 +13,7 @@ sudo pacman --sync --sysupgrade --refresh --noconfirm
 MAKEFLAGS="-J$(nproc)" makepkg --force --noconfirm --syncdeps --install --nocheck --sign
 
 curl -L -o ${REPO}.db.tar.gz http://aur.atomica.net/atomica/x86_64/${REPO}.db
+shasum -a 256 ${REPO}.db.tar.gz
 curl -L -o ${REPO}.db.tar.gz.sig http://aur.atomica.net/atomica/x86_64/${REPO}.db.sig
 repo-add --sign --verify ${REPO}.db.tar.gz *.pkg.tar.xz
 
